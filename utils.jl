a7dff1894db91fe8cf08e5a661902c6ecc0bb504
cadojo <joe.cadojo@icloud.com>
2021-01-13 21:44:18 +0000

    Deploying to gh-pages from  @ 77a2e2ed77a01ca036d6781a3f897aa174ed9329 🚀

--- utils.jl
+++ utils.jl
@@ -1,16 +1,47 @@
-function hfun_bar(vname)
-  val = Meta.parse(vname[1])
-  return round(sqrt(val), digits=2)
-end
+#
+# Utility functions for use with Franklin.jl
+#
 
-function hfun_m1fill(vname)
-  var = vname[1]
-  return pagevar("index", var)
-end
+"""
+Parses hardcoded subdirectories for blog posts, 
+and then generates headers and cards for each
+post.
 
-function lx_baz(com, _)
-  # keep this first line
-  brace_content = Franklin.content(com.braces[1]) # input string
-  # do whatever you want here
-  return uppercase(brace_content)
+__References:__  
+[1] https://franklinjl.org/syntax/utils/index.html
+"""
+function hfun_parseposts()
+
+    # Hardcode
+    ismd(f)    =  endswith(f, ".md")
+    onlymd(l)  =  filter(ismd, l)
+    onlydir(l) =  filter(isdir, l)
+    
+    # Get subdirs
+    subdirs = readdir("writing"; join=true) |> onlydir
+
+    # Get filepaths to all Markdown files
+    paths = Vector{String}()
+    for subdir ∈ subdirs
+        append!(paths, readdir(subdir; join=true) |> onlymd)
+    end
+
+    #==
+    list = readdir("blog")
+    filter!(f -> endswith(f, ".md"), list)
+    dates = [stat(joinpath("blog", f)).mtime for f in list]
+    perm = sortperm(dates, rev=true)
+    idxs = perm[1:min(3, length(perm))]
+    io = IOBuffer()
+    write(io, "<ul>")
+    for (k, i) in enumerate(idxs)
+        fi = "/blog/" * splitext(list[i])[1] * "/"
+        write(io, """<li><a href="$fi">Post $k</a></li>\n""")
+    end
+    write(io, "</ul>")
+    return String(take!(io))
+
+    ==#
+
+    paths
 end
