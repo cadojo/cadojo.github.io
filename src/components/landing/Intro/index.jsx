f8cb912016423dc9a255d7d4e11cd9519bce5efd
Joe Carpinelli <jdcarpinelli@gmail.com>
2021-01-27 22:09:47 -0500

    Minor changes to config

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -14,14 +14,14 @@ export const Intro = () => {
       <Header />
       <IntroWrapper as={Container}>
         <Details theme={theme}>
-          <h1>Hi There!</h1>
-          <h4>I’m John and I’m a JAMStack engineer!</h4>
+          <h1>Hello, world!</h1>
+          <h4>I’m Joey and I’m an aerospace controls engineer!</h4>
           <Button as={AnchorLink} href="#contact">
             Hire me
           </Button>
         </Details>
         <Thumbnail>
-          <img src={dev} alt="I’m John and I’m a JAMStack engineer!" />
+          <img src={dev} alt="I’m Joey and I’m an aerospace controls engineer!" />
         </Thumbnail>
       </IntroWrapper>
     </Wrapper>
